a36cdb742e6c3593c002a5b9cb116bda65f6cc34
Ashwani Singh <ashwanisingh034@gmail.com>
2021-09-13 17:16:12 +0530

    Heroku Setup

--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const publicpath = path.join('__dirname','..','public')
+const port = process.env.PORT || 3000;
 app.use(express.static(publicpath));
 
 app.get('*',(req,res)=>{
     res.sendFile(path.join(publicpath,'index.html'))
 })
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("server is up and Runnning!")
 })
